6a26df3d5b6f073516ef0f173579e2b2222cc4d0
Lunac <moon.zhp@gmail.com>
2024-05-11 00:04:53 +0800

    add script to bypass

---
+++ bypass-case-insensitive-in-mac.sh
@@ -0,0 +1,17 @@
+git update-index --assume-unchanged 	  linux/linux-2.6.32/linux-2.6.32/include/linux/netfilter/xt_CONNMARK.h
+git update-index --assume-unchanged 	  linux/linux-2.6.32/linux-2.6.32/include/linux/netfilter/xt_DSCP.h
+git update-index --assume-unchanged 	  linux/linux-2.6.32/linux-2.6.32/include/linux/netfilter/xt_MARK.h
+git update-index --assume-unchanged 	  linux/linux-2.6.32/linux-2.6.32/include/linux/netfilter/xt_RATEEST.h
+git update-index --assume-unchanged 	  linux/linux-2.6.32/linux-2.6.32/include/linux/netfilter/xt_TCPMSS.h
+git update-index --assume-unchanged 	  linux/linux-2.6.32/linux-2.6.32/include/linux/netfilter_ipv4/ipt_ECN.h
+git update-index --assume-unchanged 	  linux/linux-2.6.32/linux-2.6.32/include/linux/netfilter_ipv4/ipt_TTL.h
+git update-index --assume-unchanged 	  linux/linux-2.6.32/linux-2.6.32/include/linux/netfilter_ipv6/ip6t_HL.h
+git update-index --assume-unchanged 	  linux/linux-2.6.32/linux-2.6.32/net/ipv4/netfilter/ipt_ECN.c
+git update-index --assume-unchanged 	  linux/linux-2.6.32/linux-2.6.32/net/netfilter/xt_CONNMARK.c
+git update-index --assume-unchanged 	  linux/linux-2.6.32/linux-2.6.32/net/netfilter/xt_DSCP.c
+git update-index --assume-unchanged 	  linux/linux-2.6.32/linux-2.6.32/net/netfilter/xt_HL.c
+git update-index --assume-unchanged 	  linux/linux-2.6.32/linux-2.6.32/net/netfilter/xt_MARK.c
+git update-index --assume-unchanged 	  linux/linux-2.6.32/linux-2.6.32/net/netfilter/xt_RATEEST.c
+git update-index --assume-unchanged 	  linux/linux-2.6.32/linux-2.6.32/net/netfilter/xt_TCPMSS.c
+git ls-files -v | grep "^h"
+
